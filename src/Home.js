1ced9c0827cd332f68e0932456ff4b4e5d4e7a8f
rishabhjain12 <rishabhj.1222@gmail.com>
2023-05-20 22:26:59 +0530

    adding hover functionality to buttons

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import "./home.css";
 
 function Home() {
   const [count, setCount] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleIncrement = () => {
     setCount(count + 1);
@@ -16,6 +17,15 @@ function Home() {
     setCount(0);
   };
 
+  const handleHover = () => {
+    setIsHovered(true);
+
+    // Remove the hover effect after a certain duration (e.g., 1 second)
+    setTimeout(() => {
+      setIsHovered(false);
+    }, 1000);
+  };
+
   return (
     <>
       <div className="container">
@@ -23,13 +33,28 @@ function Home() {
           <h1 className="heading">Counter</h1>
           <p className="para">{count}</p>
           <div className="buttons">
-            <button className="increment btn" onClick={handleIncrement}>
+            <button
+              className={`increment btn ${isHovered ? "btn-hover" : ""}`}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+              onClick={handleIncrement}
+            >
               Increment
             </button>
-            <button className="decrement btn" onClick={handleDecrement}>
+            <button
+              className={`decrement btn ${isHovered ? "btn-hover" : ""}`}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+              onClick={handleDecrement}
+            >
               Decrement
             </button>
-            <button className="reset btn" onClick={handleReset}>
+            <button
+              className={`reset btn ${isHovered ? "btn-hover" : ""}`}
+              onMouseEnter={handleHover}
+              onMouseLeave={handleHover}
+              onClick={handleReset}
+            >
               Reset
             </button>
           </div>
